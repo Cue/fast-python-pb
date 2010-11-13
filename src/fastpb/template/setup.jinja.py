7e581faa4290cd6df01203da51884396efb710a7
Robby Walker <robby.walker@gmail.com>
2010-11-13 01:44:56 -0800

    Fix for missing library

--- src/fastpb/template/setup.jinja.py
+++ src/fastpb/template/setup.jinja.py
@@ -2,6 +2,6 @@ from distutils.core import setup, Extension
 setup(name="{{ moduleName }}", version="1.0",
       ext_modules=[
         {% for name in files %}
-          Extension("{{ name }}", ["{{ name }}.c"]),
+          Extension("{{ name }}", ["{{ name }}.c"], libraries=['protobuf']),
         {% endfor %}
       ])
