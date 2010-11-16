56cc84be5ecbffec06898a4c85884f04bcd8ad51
Robby Walker <robby.walker@gmail.com>
2010-11-16 13:44:51 -0800

    Autogenerate tests in fast-python-pb.  Pass lint.

--- src/fastpb/generator.py
+++ src/fastpb/generator.py
@@ -1,17 +1,78 @@
 #!/usr/bin/env python
+# Copyright 2010 Greplin, Inc.  All Rights Reserved.
+
+"""Generates a Python wrapper for a C++ protocol buffer."""
 
 import plugin_pb2
 
 from google.protobuf import descriptor_pb2
 from jinja2 import Template
+
+# pylint: disable=E0611
 from pkg_resources import resource_string
 
 import os.path
 import sys
-import tempfile
+
+
+TYPE = {
+  'STRING': descriptor_pb2.FieldDescriptorProto.TYPE_STRING,
+  'DOUBLE': descriptor_pb2.FieldDescriptorProto.TYPE_DOUBLE,
+  'INT64': descriptor_pb2.FieldDescriptorProto.TYPE_INT64,
+  'MESSAGE': descriptor_pb2.FieldDescriptorProto.TYPE_MESSAGE
+  # TODO(robbyw): More types.
+}
+
+LABEL = {
+  'REPEATED': descriptor_pb2.FieldDescriptorProto.LABEL_REPEATED
+}
+
+
+def template(name):
+  """Gets a template of the given name."""
+  return Template(resource_string(__name__, 'template/' + name))
+
+
+def writeCFile(response, name, fileObject):
+  """Writes a C file."""
+  context = {
+    'fileName': name,
+    'moduleName': fileObject.package.lstrip('.'),
+    'package': fileObject.package.replace('.', '::'),
+    'packageName': fileObject.package.split('.')[-1],
+    'messages': fileObject.message_type,
+    'TYPE': TYPE,
+    'LABEL': LABEL
+  }
+
+  cFile = response.file.add()
+  cFile.name = name + '.cc'
+  cFile.content = template('module.jinjacc').render(context)
+
+
+def writeSetupPy(response, files, parents):
+  """Writes the setup.py file."""
+  setupFile = response.file.add()
+  setupFile.name = 'setup.py'
+  setupFile.content = template('setup.jinjapy').render({
+    'files': files,
+    'parents': parents
+  })
+
+
+def writeTests(response, files):
+  """Writes the tests."""
+  setupFile = response.file.add()
+  setupFile.name = 'test.py'
+  setupFile.content = template('test.jinjapy').render({
+    'files': files,
+    'TYPE': TYPE,
+    'LABEL': LABEL
+  })
 
 
 def main():
+  """Main generation method."""
   request = plugin_pb2.CodeGeneratorRequest()
   request.ParseFromString(sys.stdin.read())
 
@@ -21,35 +82,18 @@ def main():
 
   generateFiles = set(request.file_to_generate)
   files = []
-  for file in request.proto_file:
-    if file.name not in generateFiles:
+  for fileObject in request.proto_file:
+    if fileObject.name not in generateFiles:
       continue
 
-    name = file.name.split('.')[0]
+    name = fileObject.name.split('.')[0]
     files.append({
-        'name': name,
-        'package': file.package.lstrip('.')
+      'name': name,
+      'package': fileObject.package.lstrip('.'),
+      'messages': fileObject.message_type
     })
 
-    context = {
-      'fileName': name,
-      'moduleName': file.package.lstrip('.'),
-      'package': file.package.replace('.', '::'),
-      'packageName': file.package.split('.')[-1],
-      'messages': file.message_type,
-      'TYPE': {
-        'STRING': descriptor_pb2.FieldDescriptorProto.TYPE_STRING,
-        'DOUBLE': descriptor_pb2.FieldDescriptorProto.TYPE_DOUBLE,
-        'INT64': descriptor_pb2.FieldDescriptorProto.TYPE_INT64,
-        'MESSAGE': descriptor_pb2.FieldDescriptorProto.TYPE_MESSAGE
-        # TODO(robbyw): More types.
-      },
-      'LABEL': {
-        'REPEATED': descriptor_pb2.FieldDescriptorProto.LABEL_REPEATED
-      }
-    }
-
-    path = file.package.lstrip('.').split('.')[:-1]
+    path = fileObject.package.lstrip('.').split('.')[:-1]
     for i in range(len(path)):
       filePathParts = path[:i+1]
       package = '.'.join(filePathParts)
@@ -61,19 +105,11 @@ def main():
         parents.add(package)
 
     # Write the C file.
-    t = Template(resource_string(__name__, 'template/module.jinjacc'))
-    cFile = response.file.add()
-    cFile.name = name + '.cc'
-    cFile.content = t.render(context)
+    writeCFile(response, name, fileObject)
 
-  # Write setup.py.
-  t = Template(resource_string(__name__, 'template/setup.jinjapy'))
-  setupFile = response.file.add()
-  setupFile.name = 'setup.py'
-  setupFile.content = t.render({
-    'files': files,
-    'parents': parents
-  })
+  writeSetupPy(response, files, parents)
+  writeTests(response, files)
+
 
   sys.stdout.write(response.SerializeToString())
 
